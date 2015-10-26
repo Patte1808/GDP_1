8b2d4f1703f1791b51ee15e0c75bb1c8509216ae
Patrick Voß <Patte@Patricks-MacBook-Air.local>
2015-10-26 23:32:55 +0100

    Abfangen von Werten kleiner null

--- src/GGT.java
+++ src/GGT.java
@@ -3,6 +3,11 @@ public class GGT {
 	
 	public static int ggT(int m, int n) {
 		
+		if(m < 0 || n < 0){
+			System.out.println("nur positive ganze Zahlen als Argumente erlaubt");
+			System.exit(-1);
+		}
+		
 		if(m < n) {
 			int tmpM = m;
 			
--- src/main.java
+++ src/main.java
@@ -2,7 +2,7 @@
 public class main {
 
 	public static void main(String[] args) {
-		System.out.println(GGT.ggT(36, 12));
+		System.out.println(GGT.ggT(-36, 12));
 
 	}
 
